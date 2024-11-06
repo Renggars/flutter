03f5bb144865eb46c75f0792739fa46d0cf88959
Rengga <renggarendy22@gmail.com>
2024-11-06 21:37:27 +0700

    belajar flutter 3

---
+++ lib/widgets/soal3.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Soal2 extends StatelessWidget {
+  const Soal2({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const FlutterLogo(),
+        title: const Text("Text Judul"),
+        centerTitle: false,
+        actions: [
+          IconButton(
+              onPressed: () {
+                // ignore: avoid_print
+                print("KLIK MORE");
+              },
+              icon: const Icon(Icons.more_vert)),
+        ],
+      ),
+      body: const Center(
+        child: FlutterLogo(
+          size: 200,
+        ),
+      ),
+    );
+  }
+}
