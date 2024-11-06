36092532b57725caeadafa70f1cee92038b357e7
Rengga <renggarendy22@gmail.com>
2024-11-06 21:15:44 +0700

    belajar flutter 1

---
+++ lib/widgets/soal1.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Soal1 extends StatelessWidget {
+  const Soal1({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const FlutterLogo(),
+        title: const Text("Text Judul"),
+        centerTitle: false,
+        actions: [
+          IconButton(
+              onPressed: () {
+                // ignore: avoid_print
+                print("KLIK MORE");
+              },
+              icon: const Icon(Icons.more_vert)),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          "Hello World",
+          style: TextStyle(
+            fontSize: 50,
+            color: Colors.blue[900],
+          ),
+        ),
+      ),
+    );
+  }
+}
