f457f15e6a221296ef0e56bc6df9bc6deb1eecab
Rengga <renggarendy22@gmail.com>
2024-11-08 21:30:21 +0700

    belajar flutter 11

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ import 'package:flutter/material.dart';
 // import 'package:flutter_application_1/widgets/soal7.dart';
 // import 'package:flutter_application_1/widgets/soal8.dart';
 // import 'package:flutter_application_1/widgets/soal9.dart';
-import 'package:flutter_application_1/widgets/soal10.dart';
-// import 'package:flutter_application_1/widgets/soal11.dart';
+// import 'package:flutter_application_1/widgets/soal10.dart';
+import 'package:flutter_application_1/widgets/soal11.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Soal10(),
+      home: Soal11(),
     );
   }
 }
