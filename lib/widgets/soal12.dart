84eb3481e9fff0d7b1ab30ad1c53bce2b9c4250f
Rengga <renggarendy22@gmail.com>
2024-11-08 21:34:59 +0700

    belajar flutter 13

--- lib/widgets/soal12.dart
+++ lib/widgets/soal12.dart
@@ -24,6 +24,7 @@ class Soal12 extends StatelessWidget {
           ],
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Row(
               children: [
